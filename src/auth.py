17185cc3b5d826cb294c05d15df4efb17898dbb4
Maxly Garcia <maxgarcia6@gmail.com>
2021-06-21 22:01:39 -0400

    Update auth.py, add templates folder.

--- src/auth.py
+++ src/auth.py
@@ -2,3 +2,15 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('login')
+def login():
+    return "<p>Login</h1>"
+
+@auth.route('logout')
+def logout():
+    return "<p>Logout</h1>"
+
+
+@auth.route('sign-up')
+def sigh_up():
+    return "<p>Sigh Uup</h1>"
