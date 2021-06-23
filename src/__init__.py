680be080b2f2ea3e0337dcc7558aa0f5295963e5
Maxly Garcia <maxgarcia6@gmail.com>
2021-06-23 00:45:59 -0400

    Add models, views, auth, db logic.

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,40 @@
 from flask import Flask
-from .views import views
-from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
-    _app = Flask(__name__)
-    _app.config['SECRET KEY'] = 'Maxsteel2!'
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'SuperSecretPass!'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
-    _app.register_blueprint(views, url_prefix='/')
-    _app.register_blueprint(auth, url_prefix='/')
+    from .views import views
+    from .auth import auth
 
-    return _app
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-    
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
